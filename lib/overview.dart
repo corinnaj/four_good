9440b3ddb219599d7bff8d55db06d6ce1aa32380
ekrebs5 <eva.krebs@student.hpi.de>
2019-05-19 08:20:13 +0200

    day bubbles

--- lib/overview.dart
+++ lib/overview.dart
@@ -73,7 +73,7 @@ class ProjectOverview extends StatelessWidget {
                                   .title
                                   .copyWith(color: Colors.white))),
                         ),
-                      );
+                      )
               ],
             ),
           ),
